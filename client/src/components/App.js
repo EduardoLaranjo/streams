5a36ef1edab432259ea435ee29cf38f93c0b0272
EduardoLaranjo <eduardolaranjo@gmail.com>
2020-04-19 12:01:23 +0100

    add Switch to routes duo to showing StreamShow in StreamCreate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import {Router} from 'react-router-dom';
+import {Router, Switch} from 'react-router-dom';
 import {Route} from 'react-router';
-import StreamShow from './streams/StreamShow';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import Header from './Header';
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import history from '../history';
+import StreamShow from './streams/StreamShow';
 
 function App() {
   return (
       <div className="ui container">
         <Router history={history}>
           <Header/>
-          <div>
+          <Switch>
             <Route path="/" exact component={StreamList}/>
             <Route path="/streams/new" exact component={StreamCreate}/>
             <Route path="/streams/edit/:id" exact component={StreamEdit}/>
-            <Route path="/streams/delete" exact component={StreamDelete}/>
-          </div>
+            <Route path="/streams/delete/:id" exact
+                   component={StreamDelete}/>
+            <Route path="/streams/:id" exact component={StreamShow}/>
+          </Switch>
         </Router>
       </div>
   );
